9c04b22ae9a794e146876c15293118d46e624379
Scott Anderson <scott.anderson@hfoh.com.au>
2019-04-28 19:43:44 +0930

    Multi-SPA V2: Then added back the files I forget to add to Git.

---
+++ Multi-SPA-V2/Angular/src/app/modules/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'angular-home',
+    templateUrl: 'home.component.html',
+    styleUrls: ['home.component.scss']
+})
+
+export class AngularHome {
+    title = 'Kitchen Sink HTML and CSS';
+}
---
+++ Multi-SPA-V2/Angular/src/app/modules/kitchensink/kitchensink.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'kitchen-sink',
+    templateUrl: 'kitchensink.component.html',
+    styleUrls: ['kitchensink.component.scss']
+})
+
+export class KitcheSink {
+    title = 'Kitchen Sink HTML and CSS';
+}
